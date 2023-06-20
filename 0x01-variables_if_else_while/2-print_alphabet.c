1c0f1135e1f6f04ef632c431e425bdaf3f8801bf
Indy907 <individuallydiale14907@gmail.com>
2023-06-20 06:17:48 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c;
 
-	for (i = 0 : i < 26 : i++)
-	{
-		putchar(alp[i]);
-	}
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
